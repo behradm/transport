ed8b45410156cd5ca466a5e9083e694b794d124b
Behrad mirafshar <behrad.nakoja@gmail.com>
2015-01-30 15:56:31 +0100

    big, bad commit

--- wp-content/themes/transport/content.php
+++ wp-content/themes/transport/content.php
@@ -5,15 +5,16 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php transport_posted_on(); ?>
-		</div><!-- .entry-meta -->
+		</div>
 		<?php endif; ?>
-	</header><!-- .entry-header -->
+	</header>
 
 	<div class="entry-content">
 		<?php
--- wp-content/themes/transport/footer.php
+++ wp-content/themes/transport/footer.php
@@ -11,11 +11,12 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<!--
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'transport' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'transport' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %1$s by %2$s.', 'transport' ), 'transport', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
+		</div>--><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
